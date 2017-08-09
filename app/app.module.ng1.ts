127550081524cf9fa21287d5667ea0e76c15592f
Christophe Krebser <christophe.krebser@leshop.ch>
2017-08-09 17:25:05 +0200

    ui-router

--- app/app.module.ng1.ts
+++ app/app.module.ng1.ts
@@ -1,8 +1,11 @@
 import * as angular from 'angular';
 import { Ng1Component } from './components/ng1.component';
 import { downgradeComponent, downgradeInjectable } from '@angular/upgrade/static';
+import 'angular-ui-router';
 
-angular.module('leshopApp', []);
+var app = angular.module('leshopApp', [
+	'ui.router'
+])
 angular.module('leshopApp')
 	.component('ng1', new Ng1Component());
 
@@ -16,3 +19,17 @@ angular.module('leshopApp')
 		component: AppComponent
 	}) as angular.IDirectiveFactory
 );
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	console.error('****', $stateProvider);
+
+	$stateProvider
+		.state('state1', {
+			url: "/state1",
+			template: require("./views/state1.html")
+		})
+		.state('state2', {
+			url: "/state2",
+			template: require("./views/state2.html")
+		});
+}]);
